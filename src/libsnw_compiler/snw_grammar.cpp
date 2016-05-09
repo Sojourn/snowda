6223aa5d3b0bbe93b476134291c606c8975805cd
Joel Rausch <jsrausch.sojourn@gmail.com>
2016-05-09 18:48:19 -0500

    Minimal grammar

--- src/libsnw_compiler/snw_grammar.cpp
+++ src/libsnw_compiler/snw_grammar.cpp
@@ -3,6 +3,76 @@
 using namespace Snowda;
 using namespace Snowda::Ast;
 
+namespace {
+    using Expr = Grammar::Expr;
+    using Nud = Grammar::Nud;
+    using Led = Grammar::Led;
+
+    ParserResult defaultNud(Parser &parser, Token token)
+    {
+        return ParserError(token, "default nud");
+    }
+
+    ParserResult defaultLed(Parser &parser, Expr expr, Token token)
+    {
+        return ParserError(token, "default led");
+    }
+
+    ParserResult errorNud(Parser &parser, Token token)
+    {
+        return ParserError(token, token.content);
+    }
+
+    ParserResult errorLed(Parser &parser, Expr expr, Token token)
+    {
+        return ParserError(token, token.content);
+    }
+}
+
 Grammar::Grammar()
+{
+    addRule(TokenType::Error, &errorNud);
+    addRule(TokenType::Error, 0, &errorLed);
+}
+
+ParserResult Grammar::nud(TokenType type, Parser &parser, Token token) const
+{
+    return getRule(type).nud(parser, token);
+}
+
+ParserResult Grammar::led(TokenType type, Parser &parser, Expr expr, Token token) const
+{
+    return getRule(type).led(parser, std::move(expr), token);
+}
+
+Grammar::Rule &Grammar::getRule(TokenType type)
+{
+    assert(type != TokenType::Count);
+    return rules_[static_cast<size_t>(type)];
+}
+
+const Grammar::Rule &Grammar::getRule(TokenType type) const
+{
+    assert(type != TokenType::Count);
+    return rules_[static_cast<size_t>(type)];
+}
+
+void Grammar::addRule(TokenType type, Nud nud)
+{
+    Rule &rule = getRule(type);
+    rule.nud = nud;
+}
+
+void Grammar::addRule(TokenType type, int bp, Led led)
+{
+    Rule &rule = getRule(type);
+    rule.bp = bp;
+    rule.led = led;
+}
+
+Grammar::Rule::Rule()
+    : bp(0)
+    , nud(&defaultNud)
+    , led(&defaultLed)
 {
 }
--- src/libsnw_compiler/snw_grammar.h
+++ src/libsnw_compiler/snw_grammar.h
@@ -7,7 +7,32 @@ namespace Snowda {
 
     class Grammar {
     public:
+        using Expr = Ast::ExpressionPtr;
+        using Nud = ParserResult(*)(Parser &, Token);
+        using Led = ParserResult(*)(Parser &, Expr, Token);
+
         Grammar();
+
+        ParserResult nud(TokenType type, Parser &parser, Token) const;
+        ParserResult led(TokenType type, Parser &parser, Expr, Token) const;
+
+    private:
+        struct Rule {
+            int bp;
+            Nud nud;
+            Led led;
+
+            Rule();
+        };
+
+        Rule &getRule(TokenType type);
+        const Rule &getRule(TokenType type) const;
+
+        void addRule(TokenType type, Nud func);
+        void addRule(TokenType type, int bp, Led func);
+
+    private:
+        Rule rules_[static_cast<size_t>(TokenType::Count)];
     };
 
 }
